801ae63ab5d27d26e398d445a0bd4892c05ac5c3
Alice Darner <alice.darner@gmail.com>
2017-03-18 12:16:06 +0100

    ajax call to SCB

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 
-
-
 document.getElementById("search_btn").onclick = function () {
     var name = document.getElementById("search_field").value;
     nameSearched(name);
@@ -10,12 +8,62 @@ function nameSearched(name) {
     setTitle(name);
     getSCBData(name);
 
+
 }
 
 function setTitle(name) {
-    document.getElementById("title_text").innerHTML = "Result for the name: " + name;
+    document.getElementById("title_text").innerHTML = "Results for the name: " + name;
 }
 
 function getSCBData(input) {
-    
+    $.support.cors = true;
+    var nameQuery = {
+        "query": [
+            {
+                "code": "Tilltalsnamn",
+                "selection": {
+                    "filter": "vs:NamnAllaFlickor",
+                    "values": [
+                        "AliceK"
+                    ]
+                }
+            },
+            {
+                "code": "Tid",
+                "selection": {
+                    "filter": "item",
+                    "values": [
+                        "2016"
+                    ]
+                }
+            }
+        ],
+        "response": {
+            "format": "json"
+        }
+    };
+    /* Yaaaay JQuery */
+    $.ajax({
+        type: "POST",
+        url: "http://api.scb.se/OV0104/v1/doris/en/ssd/BE/BE0001/BE0001T04Ar/",   
+        data: JSON.stringify(nameQuery),
+        dataType: "json",
+        success: function (data) {
+            console.log("Sucess! " + data.columns[2].text);
+        },
+        error: function (jqXHR, status, thrown) {
+            console.log("Oooops! " + status + " " + thrown);
+        }
+    });
+    showResults();
+}
+
+/* When the results are shown, the option to save should as well */
+function showResults() {
+    document.getElementById("saved_btn").style.display = "inline";
+    document.getElementById("result_table").style.display = "inline";
+}
+
+document.getElementById("saved_btn").onclick = function () {
+    document.getElementById("saved_list").style.display = "inline";
 }
