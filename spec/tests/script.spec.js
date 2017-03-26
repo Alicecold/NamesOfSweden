7fe42c896dadd38eda9d83774758e16a8862ed1a
Alice Darner <alice.darner@gmail.com>
2017-03-26 22:10:05 +0200

    I realised that my cleaning made jsdom obsolete, so it is removed

--- spec/tests/script.spec.js
+++ spec/tests/script.spec.js
@@ -2,30 +2,21 @@
 describe("Script", function () {
     var firebase = require("../../jasminMocks/firebase.js");
     var script = require("../../js/script.js");
-    var jsdom = require("jsdom");
-
-    beforeEach(function () {
-        //Call on jsdom enviroment
-        //(most of the code uses some sort of manipulation with the DOM or to Firebase,
-        // so this would make some of the tests easier to run)
-        jsdom.env("../../index.html", function (err, window) {
-            // free memory associated with the window 
-            window.close();
-        });
-
-    })
 
     it("should return true if a user is not null", function () {
+        //spy on the function containing firebase-stuff.
         spyOn(global, 'createTableFavs');
 
         //when loginstate is changed and a user exists
         var result = firebaseStateChangedToLogin({ uid: "uid", isAnonymous: true });
+
         //then return true
         expect(result).toBe(true);
         expect(createTableFavs).toHaveBeenCalled();
     });
 
     it("should return false if a user is null", function () {
+        //spy on the function containing firebase-stuff.
         spyOn(global, 'ableToSave');
 
         //when loginstate is changed and a user do not exists
